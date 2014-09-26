96ec7b917cc89a1194a42346c93550c1e19c9ea3
mcguffin <joern@flyingletters.com>
2014-09-26 19:20:29 +0200

    too many lines

--- say-cheese.php
+++ say-cheese.php
@@ -15,9 +15,6 @@ Domain Path: /lang/
 
 
 function cheese_admin_init() {
-	
-	
-	
 	wp_register_script( 'canvas-to-blob' , plugins_url( 'js/canvas-to-blob.min.js' , __FILE__ ) , array() , '2.0.5' );
 	wp_register_script( 'jquery-webcam-recorder' , plugins_url( 'js/jquery-webcam-recorder.js' , __FILE__ ) , array( 'jquery' , 'canvas-to-blob' , 'swfobject' ) , '0.0.1' );
 	wp_register_script( 'jquery-pasteboard' , plugins_url( 'js/jquery.pasteboard.js' , __FILE__ ) , array( 'jquery' ) , '0.0.1' );
