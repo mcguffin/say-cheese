ec045f8990938cb30e9afe3d4fece0ccb34a2d82
mcguffin <joern@flyingletters.com>
2014-12-28 11:44:51 +0100

    set text domain to plugin slug

--- say-cheese.php
+++ say-cheese.php
@@ -8,8 +8,6 @@ Author: Jörn Lund
 Version: 0.0.4
 Author URI: https://github.com/mcguffin/
 License: GPL2
-
-Text Domain: cheese
 */
 
 
@@ -20,19 +18,19 @@ function cheese_admin_init() {
 	wp_register_script( 'cheese' , plugins_url( 'js/cheese.js' , __FILE__ ) , array( 'jquery' ) , '0.0.1' );
 	wp_register_script( 'cheese-media-view' , plugins_url( 'js/media-view.js' , __FILE__ ) , array('media-editor' , 'jquery-webcam-recorder' , 'jquery-pasteboard' , 'cheese' ) , '0.0.1' );
 	wp_localize_script( 'cheese-media-view' , 'cheese_l10n' , array(
-		'webcam_record' => __('Webcam Record' , 'cheese' ),
-		'try_again' => __('Try again' , 'cheese' ),
-		'upload' => __('Upload' , 'cheese' ),
-		'record' => __('Record' , 'cheese' ),
+		'webcam_record' => __('Webcam Record' , 'say-cheese' ),
+		'try_again' => __('Try again' , 'say-cheese' ),
+		'upload' => __('Upload' , 'say-cheese' ),
+		'record' => __('Record' , 'say-cheese' ),
 		'title' => __('Title'),
-		'snapshot' => __('Snapshot','cheese'),
-		'copy_paste' => __('Paste from Clipboard' , 'cheese'),
+		'snapshot' => __('Snapshot','say-cheese'),
+		'copy_paste' => __('Paste from Clipboard' , 'say-cheese'),
 		'image' => __('Image'),
-		'paste_instructions' => __('Paste some image Data from your clipboard','cheese'),
-		'paste_error_no_image' => __('No image data pasted.','cheese'),
-		'paste_error_webkit_fake_image' => __('Your Browser does not support pasting processable image data.','cheese'),
-		'please_allow_camera_message' => __( 'Please allow camera access in your browser.' , 'cheese' ),
-		'an_error_occured' => __( 'An error occured.' , 'cheese' ),
+		'paste_instructions' => __('Paste some image Data from your clipboard','say-cheese'),
+		'paste_error_no_image' => __('No image data pasted.','say-cheese'),
+		'paste_error_webkit_fake_image' => __('Your Browser does not support pasting processable image data.','say-cheese'),
+		'please_allow_camera_message' => __( 'Please allow camera access in your browser.' , 'say-cheese' ),
+		'an_error_occured' => __( 'An error occured.' , 'say-cheese' ),
 		'swf_url' =>  plugins_url( 'js/WebcamRecorder.swf' , __FILE__ ),
 		'enable_snapshot' => apply_filters( 'saycheese_enable_snapshot' , true ),
 		'enable_pasteboard' => apply_filters( 'saycheese_enable_pasteboard' , true ),
@@ -43,7 +41,7 @@ function cheese_admin_init() {
 add_action( 'admin_init' , 'cheese_admin_init');
 
 function cheese_loaded() {
-	load_plugin_textdomain( 'cheese', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'say-cheese', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded' , 'cheese_loaded');
 
