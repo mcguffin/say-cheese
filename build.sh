a5e5003ac94bcb85af7732d5687022b00066046e
mcguffin <joern@flyingletters.com>
2016-03-20 14:30:34 +0100

    build script

--- build.sh
+++ build.sh
@@ -2,51 +2,55 @@
 
 export CLOSURE_COMPILER="/usr/local/compiler-latest/compiler.jar"
 
-cp ./src/adapter/adapter-latest.js ./js/
+combined=./js/cheese.min.js
+tmp_min=./js/tmp.js
 
-rm ./js/cheese.min.js
+rm $combined
+touch $combined
 
+
+# adapter latest
 java -jar \
 	$CLOSURE_COMPILER \
-	--js ./js/adapter-latest.js \
-	--js_output_file ./js/tmp.min.js
+	--js ./src/adapter/adapter-latest.js \
+	--js_output_file $tmp_min
 
-cat ./js/tmp.min.js >> ./js/cheese.min.js
-rm ./js/tmp.min.js
+cat $tmp_min >> $combined
 
 
+# recorder
 java -jar \
 	$CLOSURE_COMPILER \
 	--js ./js/jquery-webcam-recorder.js \
-	--js_output_file ./js/tmp.min.js
+	--js_output_file $tmp_min
 
-cat ./js/tmp.min.js >> ./js/cheese.min.js
-rm ./js/tmp.min.js
+cat $tmp_min >> $combined
 
 
+# pasteboard
 java -jar \
 	$CLOSURE_COMPILER \
 	--js ./js/jquery.pasteboard.js \
-	--js_output_file ./js/tmp.min.js
+	--js_output_file $tmp_min
 
-cat ./js/tmp.min.js >> ./js/cheese.min.js
-rm ./js/tmp.min.js
+cat $tmp_min >> $combined
 
-
+# cheese
 java -jar \
 	$CLOSURE_COMPILER \
 	--js ./js/cheese.js \
-	--js_output_file ./js/tmp.min.js
+	--js_output_file $tmp_min
 
-cat ./js/tmp.min.js >> ./js/cheese.min.js
-rm ./js/tmp.min.js
+cat $tmp_min >> $combined
 
-
+# media-view
 java -jar \
 	$CLOSURE_COMPILER \
 	--js ./js/media-view.js \
-	--js_output_file ./js/tmp.min.js
+	--js_output_file $tmp_min
 
-cat ./js/tmp.min.js >> ./js/cheese.min.js
-rm ./js/tmp.min.js
+cat $tmp_min >> $combined
 
+rm $tmp_min
+
+# mxmlc ./src/WebcamRecorder.mxml
