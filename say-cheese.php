7ae2ca1a1a0ec7e0c063258e6c8d34566ba9e958
mcguffin <joern@flyingletters.com>
2017-09-16 17:46:05 +0200

    version increment

--- say-cheese.php
+++ say-cheese.php
@@ -3,9 +3,9 @@
 /*
 Plugin Name: Say Cheese
 Plugin URI: https://github.com/mcguffin/say-cheese/
-Description: Add Media through Webcam and Copy Paste
+Description: Add Media through Webcam recording
 Author: Jörn Lund
-Version: 0.4.0
+Version: 0.4.1
 Author URI: https://github.com/mcguffin/
 License: GPL2
 */
@@ -14,7 +14,7 @@ License: GPL2
 namespace SayCheese;
 
 define( 'SAY_CHEESE_FILE', __FILE__ );
-define( 'SAY_CHEESE_VERSION', '0.3.1' );
+define( 'SAY_CHEESE_VERSION', '0.4.1' );
 define( 'SAY_CHEESE_DIRECTORY', plugin_dir_path(__FILE__) );
 
 require_once SAY_CHEESE_DIRECTORY . 'include/vendor/autoload.php';
